02651a9e3f9cae2122b159484160e35f3b22108b
joaquimoiio <joaquimpaes01@gmail.com>
2025-04-24 16:07:59 -0300

    24/04

--- src/main/java/com/projetobackend/demo/controllers/PedidoController.java
+++ src/main/java/com/projetobackend/demo/controllers/PedidoController.java
@@ -16,7 +16,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
